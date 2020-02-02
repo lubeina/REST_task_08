bd77cb1f7d66b0a65600d3f2cfb2eb08cf2797c9
lubeina <52536093+lubeina@users.noreply.github.com>
2020-02-02 17:08:36 +0300

    Task Complete

--- flights/serializers.py
+++ flights/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from datetime import date
 
 from .models import Flight, Booking, Profile
 
@@ -12,14 +13,26 @@ class FlightSerializer(serializers.ModelSerializer):
 
 class BookingSerializer(serializers.ModelSerializer):
 	class Meta:
+		tracks = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='destination')
+
 		model = Booking
 		fields = ['flight', 'date', 'id']
 
 
 class BookingDetailsSerializer(serializers.ModelSerializer):
+	total = serializers.SerializerMethodField()
+	flight = FlightSerializer()
 	class Meta:
 		model = Booking
-		fields = ['flight', 'date', 'passengers', 'id']
+		fields = ['flight', 'date', 'passengers', 'id','total']
+
+	def get_total(self, obj):
+		cost = obj.flight.price
+		total = cost * obj.passengers
+		return total
+
 
 
 class AdminUpdateBookingSerializer(serializers.ModelSerializer):
@@ -35,24 +48,54 @@ class UpdateBookingSerializer(serializers.ModelSerializer):
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-    class Meta:
-        model = User
-        fields = ['username', 'password', 'first_name', 'last_name']
+	password = serializers.CharField(write_only=True)
+	class Meta:
+		model = User
+		fields = ['username', 'password', 'first_name', 'last_name']
 
-    def create(self, validated_data):
-        username = validated_data['username']
-        password = validated_data['password']
-        first_name = validated_data['first_name']
-        last_name = validated_data['last_name']
-        new_user = User(username=username, first_name=first_name, last_name=last_name)
-        new_user.set_password(password)
-        new_user.save()
-        return validated_data
+	def create(self, validated_data):
+		username = validated_data['username']
+		password = validated_data['password']
+		first_name = validated_data['first_name']
+		last_name = validated_data['last_name']
+		new_user = User(username=username, first_name=first_name, last_name=last_name)
+		new_user.set_password(password)
+		new_user.save()
+		return validated_data
+
+class UserSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = ['first_name','last_name']
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+	user = UserSerializer()
+	past_bookings = serializers.SerializerMethodField()
+	tier = serializers.SerializerMethodField()
+
 	class Meta:
+
 		model = Profile
-		fields = ['user', 'miles']
+		fields = ['user', 'miles','past_bookings','tier']
+
+	def get_past_bookings(self, obj):
+		bookings = Booking.objects.filter(user = obj.user,date__lt = date.today())
+		serializer = BookingSerializer(bookings,many = True)
+		return serializer.data
+
+	def get_user(self, obj):
+		return f"{obj.user.first_name} {obj.user.last_name}"
+
+	def get_tier(self,obj):
+		if obj.miles < 9999 :
+			return "Blue"
+		elif obj.miles >= 10000 and obj.miles <= 60000 :
+			return "Silver"
+		elif obj.miles >= 60000 and obj.miles <= 100000 :
+			return "Gold"
+		elif obj.miles > 100000 :
+			return "Platinum"
+
+
 
